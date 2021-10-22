d94136ba4276d2eff491bbe472ec6f1bf3d1dfd8
aldubar <aldubar@student.42.fr>
2021-10-22 23:37:53 +0200

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,18 @@
 if [ -f Makefile ] ; then
 	ARGS="$1"
 	TARGET=$( cat Makefile | grep -m1 NAME | sed 's/[[:space:]]//g' | cut -d= -f2 )
-	if [ ${ARGS} = "check" ] ; then
+	if [ "$ARGS" = "check" ] ; then
 		make check
 	else
 		make
-		[ $? -eq 0 ] && valgrind ./${TARGET} ${ARGS}
+		[[ $? -eq 0 ]] && valgrind ./${TARGET} ${ARGS}
 	fi
 else
 	FILES="$1"
 	ARGS="$2"
 	TARGET="a.out"
 	clang -Wall -Wextra -Werror "$FILES" -o "${TARGET}"
-	[ $? -eq 0 ] && valgrind ./${TARGET} ${ARGS}
+	[[ $? -eq 0 ]] && valgrind ./${TARGET} ${ARGS}
 fi
 
 
